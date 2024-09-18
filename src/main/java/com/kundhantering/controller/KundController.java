a128ccfba6de80c72c3c0709c534ad196a07ee3c
Frida Ulfves <frida_ulfves@hotmail.com>
2024-09-18 10:42:28 +0200

    updated controller

--- src/main/java/com/kundhantering/controller/KundController.java
+++ src/main/java/com/kundhantering/controller/KundController.java
@@ -3,7 +3,11 @@ package com.kundhantering.controller;
 import com.kundhantering.model.Kund;
 import com.kundhantering.service.KundService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Collections;
+
 
 import java.util.List;
 
@@ -14,15 +18,29 @@ public class KundController {
     @Autowired
     private KundService kundService;
 
-    // lägg till ny kund
-    @PostMapping("/user")
-    public void addKund(@RequestBody Kund kund) {
-        kundService.addKund(kund);
+    // Lägg till ny kund
+    @PostMapping
+    public ResponseEntity<String> addKund(@RequestBody Kund kund) {
+        try {
+            kundService.addKund(kund);
+            return new ResponseEntity<>("Kund tillagd", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Kunde inte lägga till kund", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    // hämta alla kunder
-    @GetMapping("/user")
-    public List<Kund> getAllKunder() {
-        return kundService.getAllKunder();
+    // Hämta alla kunder med paginering
+    @GetMapping
+    public List<Kund> getAllaKunder(@RequestParam(defaultValue = "0") int page, 
+                                   @RequestParam(defaultValue = "10") int size) {
+        List<Kund> allaKunder = kundService.getAllaKunder();
+        int start = page * size;
+        if (start >= allaKunder.size()) {
+
+            // returnera en oföränderlig tom lista
+            return Collections.emptyList(); // Om det inte finns fler kunder, returnera en tom lista
+        }
+        int end = Math.min((start + size), allaKunder.size());
+        return allaKunder.subList(start, end);
     }
 }
