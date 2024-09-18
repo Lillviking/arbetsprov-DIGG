4303bf7523376b2420878571de066eea88dc1275
Frida Ulfves <frida_ulfves@hotmail.com>
2024-09-18 10:43:05 +0200

    styled homepage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,75 +3,93 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <header>
+            <div class="sidenav">
+                <RouterLink to="/" class="nav-text">Kundlista</RouterLink>
+                <RouterLink to="/addCustomer" class="nav-text">Lägg till kund</RouterLink>
+                <div class="empty-div"></div>
+                <div class="empty-div2"></div>
+                <div class="empty-div3"></div>
+                <div class="empty-div4"></div>
+                <div class="empty-div5"></div>
+            </div>
+    </header>
+    <div class="content"><RouterView /></div>
+    
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-  background-color: rgb(169, 179, 179);
-}
 
-nav {
-  width: 100%;
-  text-align: center;
-  margin-top: 2rem;
-  padding: 2rem;
-  font-weight: 700;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+.sidenav {
+    height: 100%;
+    width: 20rem;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #695F59;
+    overflow-x: hidden;
+    padding-top: 3rem;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    flex-direction: column;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.nav-text {
+    color: white;
+    font-weight: bold;
+    margin-top: 2rem;
+    padding-left: 3rem;
+}
 
-  header .wrapper {
+.nav-text:nth-of-type(2) {
+    margin-bottom: 8rem; 
+}
+
+.content {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+.empty-div {
+    height: 40px;
+    width: 100%;
+    background-color: #EBC0B8;
+    flex-grow: 1;
+}
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.empty-div2 {
+    height: 40px;
+    width: 100%;
+    background-color: #D6D9D3;
+    flex-grow: 2;
+}
+
+.empty-div3 {
+    height: 40px;
+    width: 100%;
+    background-color: #F0EFEE;
+    flex-grow: 3;
+}
+
+.empty-div4 {
+    height: 40px;
+    width: 100%;
+    background-color: #CD7A6E;
+    flex-grow: 1;
+}
+
+.empty-div5 {
+    height: 40px;
+    width: 100%;
+    background-color: #F7E4E1;
+    flex-grow: 4;
+}
+
+a {
+    text-decoration: none;
+    font-size: 2rem;
 }
 </style>
