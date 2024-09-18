cfa95e248cd8739acf356b85c6032c4f9f0e37b9
Frida Ulfves <frida_ulfves@hotmail.com>
2024-09-18 10:40:31 +0200

    AddCustomersView and CustomersView

---
+++ frontend/src/views/AddCustomerView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AddCustomer from '@/components/AddCustomer.vue';
+
+</script>
+
+<template>
+    <div>
+        <AddCustomer /> 
+    </div>
+</template>
---
+++ frontend/src/views/CustomersView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Customers from '@/components/Customers.vue';
+</script>
+
+<template>
+  <div class="about">
+    <Customers />
+  </div>
+</template>
+
